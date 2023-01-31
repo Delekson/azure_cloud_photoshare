5ac6e595c53d5c4e47e9e18a62548b92a2c98561
Delekson <manikavasakard@gmail.com>
2023-01-31 09:43:58 +0000

    minor tweaks and formatting changes

---
+++ config_files.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from dataclasses import dataclass
+
+load_dotenv(find_dotenv())
+@dataclass(frozen=True)
+class vars:
+    private_key: str = os.getenv('account_key')
+    account_name: str = os.getenv('account_name')
+    container_name: str = os.getenv('container_name')
